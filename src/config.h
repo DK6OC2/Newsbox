af1f8506a7398130b89bc71311f00f60bd7bf31e
DD1NK <97752436+DD1NK@users.noreply.github.com>
2024-02-11 13:50:42 +0100

    Aufräumen nach Umstellung auf DHCP und "unsicheres" HTTPS

--- src/config.h
+++ src/config.h
@@ -1,14 +1,6 @@
 //Enter personal, Wifi and Message source configuration below
-const char* WIFI_SSID = "YourSSID";                  // Name des eigenen WLAN
-const char* WIFI_PASSWORD = "Yourpass"; // Passwort eigenes WLAN
-const char* host = "www.r04-afu.de";                // Link zum OV-Server
-const String URL = "https://www.r04-afu.de/info.txt";
-const String OV = "News";          // Individueller Text
-const String Rufzeichen = "YourCall"; // eigenes Call
+const char* WIFI_SSID = "Homenet";                  // Name des eigenen WLAN
+const char* WIFI_PASSWORD = "nichtzuknacken"; // Passwort eigenes WLAN
+const String URL = "https://www.r04-afu.de/info.txt"; //Link zum Server
+const String Rufzeichen = "DD1NK"; // eigenes Call
 const String Locator = "JO31kd"; // eigener Locator
-
-IPAddress ip(192, 168, 178, 72);       // feste IP-Adresse im eigenen Netz
-IPAddress gateway(192, 168, 178, 1);  // Gateway-Adresse im eigenen Netz
-IPAddress subnet(255, 255, 255, 0); // Subnet IP-Adresse im eigenen Netz
-IPAddress primaryDNS(8, 8, 8, 8); // optional
-IPAddress secondaryDNS(8, 8, 4, 4); // optional
--- src/main.cpp
+++ src/main.cpp
@@ -8,45 +8,13 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);                 // für andere Displays oder // Adressen anzupassen
 
 const int DISPLAY_WIDTH = 16; // Definition fürs Display
+byte mac[6];   // byte-array für Mac-Adresse
 String payload; // Variable für Nachricht
 String old_payload = "empty"; // Variable für Änderungsprüfung, muss beim ersten Durchlauf abweichen
 String MacAddr;
 int payload_length; // Variable für die Länge des Textes vom Server definieren
 unsigned long startTime = 0; //startpunkt für Zeitschleife
 unsigned long interval  = 600000 ; //Nachricht aller 10m abfragen
-//SSL-Zertifikat
-//const char* rootCACertificate = \
-     "-----BEGIN CERTIFICATE-----\n" \
-     "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
-     "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-     "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
-     "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
-     "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
-     "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
-     "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
-     "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
-     "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
-     "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
-     "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
-     "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
-     "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
-     "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
-     "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
-     "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
-     "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
-     "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
-     "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
-     "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
-     "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
-     "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
-     "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
-     "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
-     "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
-     "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
-     "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
-     "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
-     "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
-     "-----END CERTIFICATE-----\n";
 
 bool fetchmessage = true ; // Flag für Zeitschleife des Nachrichtenabrufs
 WiFiClientSecure *client = new WiFiClientSecure ; // initialisieren des WifiClients mit SSL
@@ -62,16 +30,6 @@ void setup()
   //lcd.begin(0, 2); //je nach Display ggf. statt init()
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print(Rufzeichen);
-  lcd.setCursor(10, 0);
-  lcd.print(Locator);
-  lcd.setCursor(0, 1);
-  lcd.print(OV);
-//WiFi-Parameter setzen und verbinden
-// if (!WiFi.config(ip, gateway, subnet, primaryDNS, secondaryDNS)) {
-// Serial.println("STA Failed to configure");
-}
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED)
@@ -89,19 +47,18 @@ void setup()
   MacAddr += String(mac[2],HEX);
   MacAddr += String(mac[1],HEX);
   MacAddr += String(mac[0],HEX);
-  Serial.Println("MAC: "+MacAddr);
+  Serial.println("MAC: "+MacAddr);
   lcd.setCursor(0, 0);
   lcd.print(Rufzeichen);
   lcd.setCursor(10, 0);
   lcd.print(Locator);
   lcd.setCursor(0, 1);
   lcd.print("Mac: "+MacAddr);
-  delay(2000);
+  delay(3000);
 }
 
 void loop()
 {
-  //client->setCACert(rootCACertificate); // Set Server-Certificate
   client->setInsecure(); // ignoriere SSL-Cert
   
   HTTPClient http ; //Webclient starten
