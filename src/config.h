c119a8474fc17b27c3d8d8758b5d099d17588d7a
dl0dx <dl0dx@gmx.de>
2024-02-18 13:04:29 +0100

    Call im GET-Request

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 //Enter personal, Wifi and Message source configuration below
-const char* WIFI_SSID = "Homenet";                  // Name des eigenen WLAN
-const char* WIFI_PASSWORD = "nichtzuknacken"; // Passwort eigenes WLAN
-const String URL = "https://newsbox.r04-afu.de/info.txt"; //Link zum Server
+const char* WIFI_SSID = "DL0DX";                  // Name des eigenen WLAN
+const char* WIFI_PASSWORD = "r01-jo31jf"; // Passwort eigenes WLAN
+const String URL = "https://newsbox.r04-afu.de/news.json"; //Link zum Server
 const String Rufzeichen = "DD1NK"; // eigenes Call
 const String Locator = "JO31kd"; // eigener Locator
