91a065b810176fd4c922f512ff13df71fc7f84cd
DD1NK <97752436+DD1NK@users.noreply.github.com>
2024-04-04 20:53:10 +0200

    Displayfile für 20/4-Display und Teplate für Displayfile

---
+++ src/displays/display_template.h
@@ -0,0 +1,37 @@
+//general display setup
+#include ....
+//weiterer setupcode
+
+void init_display()
+{
+  // code für Displayinitialisierung
+}
+void display_splash()
+{
+  //code für splash screen, wenn gewüncht
+}
+void display_boxdata()
+{
+  //Anzeige der Daten der Box (Rufzeichen, Locator, MacAdresse)
+}
+void display_wifi_connecting()
+{
+   //code für connecting, wenn gewünscht
+}
+void display_wifi_connected()
+{
+   //code für die Anzeige der IP-Adresse nach Verbindung zum WLAN
+   
+}
+void display_fetch_flag()
+{
+   //code für Abrufsignal
+}
+void remove_fetch_flag()
+{
+   //code für Entfernung des Abrufsignals
+}
+void display_message()
+{
+   //code für das Anzeigen einer Nachricht
+}
---
+++ src/displays/lcd2004.h
@@ -0,0 +1,61 @@
+//general display setup
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);                 
+void init_display()
+{
+  // code für Displayinitialisierung
+  lcd.init(I2C_SDA, I2C_SCL);
+  lcd.backlight();
+}
+void display_splash()
+{
+  //code für splash screen, wenn gewüncht
+}
+void display_boxdata()
+{
+  //Anzeige der Daten der Box (Rufzeichen, Locator, MacAdresse)
+  lcd.setCursor(0, 0);
+  lcd.print("Newsbox-Projekt");
+  lcd.setCursor(0,1);
+  lcd.print(Rufzeichen);
+  lcd.setCursor(10, 1);
+  lcd.print(Locator);
+  lcd.setCursor(0, 2);
+  lcd.print("Mac: "+MacAddr);
+}
+void display_wifi_connecting()
+{
+   //code für connecting, wenn gewünscht
+}
+void display_wifi_connected()
+{
+   //code für die Anzeige der IP-Adresse nach Verbindung zum WLAN
+   
+}
+void display_fetch_flag()
+{
+   //code für Abrufsignal
+   lcd.setCursor(0, 9); 
+   lcd.print("*");
+}
+void remove_fetch_flag()
+{
+   //code für Entfernung des Abrufsignals
+   lcd.setCursor(0, 9);
+   lcd.print(" ");
+}
+void display_message()
+{
+   //code für das Anzeigen einer Nachricht
+   lcd.clear();  
+   lcd.setCursor(0, 0);
+   lcd.print(news_topic);
+   lcd.setCursor(10, 0);
+   lcd.print(news_date); 
+   lcd.setCursor(0, 1);
+   lcd.print(news_line1);
+   lcd.setCursor(0, 2);
+   lcd.print(news_line2);
+   lcd.setCursor(0, 3);
+   lcd.print(news_line3);
+}
