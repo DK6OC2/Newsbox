2e4506dcd949d9d01fc47b2ee17f89c164753bc5
Dieter Eggert Hinsch <142132299+DK6OC2@users.noreply.github.com>
2024-09-16 17:09:06 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 //Enter personal, Wifi and Message source configuration below
-const char* WIFI_SSID1 = "DL0DX";                  // Name des eigenen WLAN1
-const char* WIFI_PASSWORD1 = "r01-jo31jf";        // Passwort eigenes WLAN1
+const char* WIFI_SSID1 = "SSID1";                  // Name des eigenen WLAN1
+const char* WIFI_PASSWORD1 = "PW1";        // Passwort eigenes WLAN1
 const char* WIFI_SSID2 = "Homenet";               // Name des eigenen WLAN2
 const char* WIFI_PASSWORD2 = "nichtzuknacken";    // Passwort eigenes WLAN2
 //more WLAN-Data if applicable with increasing number 
@@ -11,9 +11,9 @@ const char* WIFI_PASSWORD4 = "PW4";                // Passwort eigenes WLAN4
 const char* WIFI_SSID5 = "SSID5";                    // Name des eigenen WLAN4
 const char* WIFI_PASSWORD5 = "PW5";                // Passwort eigenes WLAN4
 
-const String URL = "https://newsbox.r04-afu.de/news.json"; //Link zum Server
-const char* Rufzeichen = "DD1NK"; // eigenes Call
-const char* Locator = "JO31kd"; // eigener Locator
+const String URL = "https://Dein Server/news.json"; //Link zum Server hier eintragen
+const char* Rufzeichen = "CALL"; // eigenes Call
+const char* Locator = "JO31xx"; // eigener Locator
 
 //Hardware configuration, jeweils eine Alternative wählen
 
